c082a869c0eda759faf24ef2ac1d023cd5e58d42
WilerMS <wilersoriano@gmail.com>
2024-06-13 20:51:48 +0200

    add path aliases and configure proxy

--- app/packages/web/vite.config.ts
+++ app/packages/web/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    proxy:{
+      '/api': {
+        target: 'http://localhost:3000/api',
+        changeOrigin: true
+      }
+    }
+  }
 })
