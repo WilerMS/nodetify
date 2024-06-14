d2e035b43d3ebc87cb6174cb8a106dbb35c6be8c
WilerMS <wilersoriano@gmail.com>
2024-06-14 15:54:28 +0200

    fix errors in run.sh

--- run.sh
+++ run.sh
@@ -4,12 +4,13 @@ if [ "$1" == "--dev" ]; then
     COMPOSE_FILE="docker-compose-dev.yml"
     cd app/
     npm install || { echo "Error during npm install execution"; exit 1; }
+    cd ../docker
 elif [ "$1" == "--prod" ]; then
     COMPOSE_FILE="docker-compose.yml"
+    cd ./docker
 else
     echo "Missing deploy mode flag: $0 --dev | --prod"
     exit 1
 fi
 
-cd ../docker
-docker-compose -f "$COMPOSE_FILE" up || { echo "Error during docker-compose execution"; exit 1; }
+docker-compose -f "$COMPOSE_FILE" up --build || { echo "Error during docker-compose execution"; exit 1; }
