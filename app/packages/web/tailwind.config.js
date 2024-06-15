df9faa67383834800fe59d2c58967f6f1946b88b
WilerMS <wilersoriano@gmail.com>
2024-06-15 15:48:49 +0200

    fix error in tailwind config with node modules

--- app/packages/web/tailwind.config.js
+++ app/packages/web/tailwind.config.js
@@ -1,23 +1,15 @@
-import { nextui } from '@nextui-org/react'
-
+import { nextui } from "@nextui-org/react"
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
-    screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1500px',
-      '2xl': '1936px'
-    }
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [nextui()]
 }
