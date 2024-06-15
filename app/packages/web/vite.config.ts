8c1be84adac4cdbe64fa41cf163e057a88d6657f
WilerMS <wilersoriano@gmail.com>
2024-06-16 01:01:06 +0200

    add tankstack router

--- app/packages/web/vite.config.ts
+++ app/packages/web/vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(), 
+    tsconfigPaths(), 
+    TanStackRouterVite()
+  ],
   server: {
     proxy:{
       '/api': {
