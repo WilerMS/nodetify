65ac0ff5d8bec35c55b6db62cdc040334d37a6cb
WilerMS <wilersoriano@gmail.com>
2024-06-16 15:53:34 +0200

    Create a logo component

---
+++ app/packages/web/src/components/ui/Logo.tsx
@@ -0,0 +1,21 @@
+import { IconBell } from '@tabler/icons-react'
+import { type FC } from 'react'
+import cn from 'classnames'
+
+interface LogoProps {
+  showTitle?: boolean
+  className?: string
+}
+
+export const Logo: FC<LogoProps> = ({ className, showTitle = true }) => {
+  return (
+    <div className={cn('w-full h-full px-6 flex items-center cursor-pointer', className)}>
+      <div className="logo flex gap-2 items-center">
+        <div className='bg-black w-[40px] h-[40px] center rounded-lg'>
+          <IconBell className='text-white -rotate-12' size={30} stroke={1.8}/>
+        </div>
+        {showTitle && <span className='text-2xl transition-all duration-1000 font-bold'>Nodetify</span>}
+      </div>
+    </div>
+  )
+}
