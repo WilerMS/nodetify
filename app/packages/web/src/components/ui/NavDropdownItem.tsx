41fe1e04def19673f6b57dc96573378079c129cc
WilerMS <wilersoriano@gmail.com>
2024-06-16 15:53:54 +0200

    dropdown for menu

--- app/packages/web/src/components/ui/NavDropdownItem.tsx
+++ app/packages/web/src/components/ui/NavDropdownItem.tsx
@@ -5,19 +5,26 @@ import { NavItem } from './NavItem'
 
 interface NavDropdownItemProps extends React.PropsWithChildren {
   icon?: React.ReactNode
-  title: React.ReactNode
+  title: string
   defaultOpen?: boolean
+  collapsed?: boolean
+  onExpand?: (value: boolean) => void
 }
 
 export const NavDropdownItem: FC<NavDropdownItemProps> = ({
   icon,
   title,
   defaultOpen,
+  collapsed = false,
+  onExpand,
   children
 }) => {
   const [isExpanded, setIsExpanded] = useState(defaultOpen)
 
-  const handleToggle = (value: boolean) => setIsExpanded(value)
+  const handleToggle = (value: boolean) => {
+    setIsExpanded(value)
+    if (value) onExpand?.(value)
+  }
 
   return (
     <Collapse
@@ -25,17 +32,22 @@ export const NavDropdownItem: FC<NavDropdownItemProps> = ({
         <NavItem>
           <div className='w-full flex-shrink-0 flex items-center gap-2'>
             <div>{icon}</div>
-            {title}
-            <div className='w-full flex justify-end'>
-              {isExpanded
-                ? <IconChevronDown size={20} />
-                : <IconChevronLeft size={20} />
-              }
-            </div>
+            {!collapsed && (
+              <>
+                <span>{title}</span>
+                <div className='w-full flex justify-end'>
+                  {isExpanded
+                    ? <IconChevronDown size={20} />
+                    : <IconChevronLeft size={20} />
+                  }
+                </div>
+              </>
+            )}
           </div>
         </NavItem>
       }
       defaultOpen={defaultOpen}
+      forceClosed={collapsed}
       onToggle={handleToggle}
     >
       {children}
