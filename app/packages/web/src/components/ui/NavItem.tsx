7e95f618abbd956d9d8a8b2e0c1899928ef073d4
WilerMS <wilersoriano@gmail.com>
2024-06-16 15:54:18 +0200

    nav item and nav link modified

--- app/packages/web/src/components/ui/NavItem.tsx
+++ app/packages/web/src/components/ui/NavItem.tsx
@@ -3,20 +3,24 @@ import cn from 'classnames'
 
 interface NavItemProps extends React.PropsWithChildren {
   isActive?: boolean
+  className?: string
 }
 
-export const NavItem: FC<NavItemProps> = ({ isActive, children, ...props }) => {
+export const NavItem: FC<NavItemProps> = ({ isActive, className, children, ...props }) => {
   return (
     <div
       className={cn(
-        'flex items-center gap-2 py-3 px-3 rounded-lg truncate text-gray-700',
+        'flex items-center gap-4 py-3 px-3 rounded-lg truncate relative',
         'active:scale-95 transition-all duration-200',
         {
-          'hover:bg-gray-200': !isActive,
-          'bg-blue-100': isActive,
+          'hover:bg-gray-100': !isActive,
+          'bg-gray-200': isActive,
           'font-bold': isActive,
-          'font-normal': !isActive
-        }
+          'font-normal': !isActive,
+          'text-gray-600': !isActive,
+          'text-black': isActive
+        },
+        className
       )}
       {...props}
     >
--- app/packages/web/src/components/ui/NavLink.tsx
+++ app/packages/web/src/components/ui/NavLink.tsx
@@ -1,19 +1,37 @@
 import { type FC } from 'react'
 import { Link, type LinkProps } from '@tanstack/react-router'
+import cn from 'classnames'
 import { NavItem } from './NavItem'
 
 interface NavLinkProps extends LinkProps {
   icon?: React.ReactNode
-  children: React.ReactNode
+  title: React.ReactNode
+  showTitle?: boolean
 }
 
-export const NavLink: FC<NavLinkProps> = ({ icon, children, ...props }) => {
+export const NavLink: FC<NavLinkProps> = ({
+  icon,
+  title,
+  showTitle = true,
+  ...props
+}) => {
   return (
     <Link {...props}>
       {({ isActive }) => (
-        <NavItem isActive={isActive}>
-          {icon}
-          {children}
+        <NavItem
+          className={cn({
+            // center: !showTitle
+          })}
+          isActive={isActive}
+        >
+          <span className='flex-shrink-0'>
+            {icon}
+          </span>
+          {showTitle &&
+            <span>
+              {title}
+            </span>
+          }
         </NavItem>
       )}
     </Link>
