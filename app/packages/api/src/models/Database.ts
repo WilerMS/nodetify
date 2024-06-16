ec6fdfb853be727e5852a611e9ebfb4c0f2cabeb
WilerMS <wilersoriano@gmail.com>
2024-06-17 00:01:55 +0200

    add database objection  model

---
+++ app/packages/api/src/models/Database.ts
@@ -0,0 +1,45 @@
+import { Model } from '@/config/knex'
+import { type DatabaseConnection } from '@/types/global'
+
+export class Database extends Model {
+  static tableName = 'database'
+  static idColumn = 'id'
+
+  id!: number
+  name!: string
+  description?: string
+  type!: 'PostgreSQL' | 'MySQL' | 'MariaDB' | 'SQLite' | 'SQLServer' | 'Oracle'
+  connection!: DatabaseConnection
+  created_at!: string
+  updated_at!: string
+
+  static get jsonSchema () {
+    return {
+      type: 'object',
+      required: ['name', 'type', 'connection'],
+      properties: {
+        id: { type: 'integer' },
+        name: { type: 'string', maxLength: 255 },
+        description: { type: 'string' },
+        type: { type: 'string', enum: ['PostgreSQL', 'MySQL', 'MariaDB', 'SQLite', 'SQLServer', 'Oracle'] },
+        connection: {
+          type: 'object',
+          properties: {
+            host: { type: 'string' },
+            port: { type: 'integer' },
+            database: { type: 'string' },
+            user: { type: 'string' },
+            password: { type: 'string' }
+          },
+          required: ['host', 'port', 'database', 'user', 'password']
+        },
+        created_at: { type: 'string', format: 'date-time' },
+        updated_at: { type: 'string', format: 'date-time' }
+      }
+    }
+  }
+
+  static get relationMappings () {
+    return {}
+  }
+}
