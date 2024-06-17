1ccc38287530d3244dece62d9ec83991367501d3
WilerMS <wilersoriano@gmail.com>
2024-06-18 00:21:01 +0200

    add typed authenticated request

--- app/packages/api/src/types/global.ts
+++ app/packages/api/src/types/global.ts
@@ -1,7 +1,16 @@
+import { type Request } from 'express'
+import { type User } from '@/models'
+
 export interface DatabaseConnection {
   host: string
   port: number
   database: string
   user: string
   password: string
+}
+
+export interface AuthenticatedRequest extends Request {
+  auth?: {
+    user: User
+  }
 }
