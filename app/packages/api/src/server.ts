9ed174f7e4eef05a8eaac5ddf8f97b4a6c550638
WilerMS <wilersoriano@gmail.com>
2024-06-19 01:31:22 +0200

    add cookie parser to server

--- app/packages/api/src/server.ts
+++ app/packages/api/src/server.ts
@@ -1,8 +1,9 @@
 import './config-paths'
+import path from 'path'
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
-import path from 'path'
+import cookieParser from 'cookie-parser'
 
 import { PORT } from '@/config/env'
 import { errorMiddleware } from '@/middlewares'
@@ -14,6 +15,7 @@ const app = express()
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
+app.use(cookieParser())
 
 // static files
 const publicUrl = path.join(__dirname, '../../', 'web/dist')
