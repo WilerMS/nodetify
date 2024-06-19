b7ceb692d2f1bd3aa7bb39f5161871ef4f90c4bf
WilerMS <wilersoriano@gmail.com>
2024-06-20 00:34:08 +0200

    login page finished. I still have to conect it to the backend

--- app/packages/web/src/routes/(auth)/login.tsx
+++ app/packages/web/src/routes/(auth)/login.tsx
@@ -3,9 +3,17 @@ import { Button } from '@nextui-org/react'
 import { Link, createFileRoute } from '@tanstack/react-router'
 
 import { InputPassword, InputText, Logo } from '@/components/ui'
-import { IconGoogle, IconFacebook } from '@/components/icons'
+import { IconGoogle, IconGithub } from '@/components/icons'
 
 const Login: FC = () => {
+
+  const handleSubmitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const data = Object.fromEntries(formData.entries())
+    console.log({ data })
+  }
+
   return (
     <div className='w-full h-full flex bg-gray-700'>
       <main className='w-full h-full center'>
@@ -16,41 +24,40 @@ const Login: FC = () => {
           <div className="w-full h-full center">
             <div className='w-full'>
               <div className="w-full gap-4 flex-col">
-                <h1 className='text-3xl font-light'>Log in into your Account</h1>
+                <h1 className='text-3xl font-light'>Log in into your account</h1>
                 <p className='text-sm text-gray-700 mt-2'>Welcome back! Select a method to log in</p>
               </div>
               <div className="w-full mt-10 flex items-center justify-center flex-col lg:flex-row gap-4">
-                <Button className='w-full h-[45px]' color="default" variant="solid">
-                  <IconGoogle width={20} height={20} className='flex-shrink-0 mr-1' />
+                <Button className='w-full h-[45px]' color="default" variant="faded">
+                  <IconGoogle width={18} height={18} className='flex-shrink-0 mr-1' />
                   <span className='font-bold'>Google</span>
                 </Button>
-                <Button className='w-full h-[45px]' color="default" variant="solid">
-                  <IconFacebook width={20} height={20} className='flex-shrink-0' />
-                  <span className='font-bold'>Facebook</span>
+                <Button className='w-full h-[45px]' color="default" variant="faded">
+                  <IconGithub width={18} height={18} className='flex-shrink-0' />
+                  <span className='font-bold'>Github</span>
                 </Button>
               </div>
-              <div className="w-full mt-6 text-gray-600 center">
-                <p className='text-sm'>or continue with username</p>
-              </div>
-              <div className='w-full mt-6 center gap-4 flex-col'>
-                <InputText label='Username' />
-                <InputPassword />
+              <p className='text-center text-sm mt-6 text-gray-600'>or continue with username</p>
+              <form onSubmit={handleSubmitForm} className='w-full mt-6 center gap-4 flex-col'>
+                <InputText label='Username' name='username' className='[&>div]:border-gray-300' />
+                <InputPassword name='password' className='[&>div]:border-gray-300' />
                 <div className="w-full flex justify-end items-center">
-                  <Link to='/register' className='font-bold'>Forgot your password?</Link>
+                  <span className='cursor-pointer font-bold'>Forgot your password?</span>
                 </div>
                 <Button
-                  className='w-full h-[45px]'
+                  className='w-full h-[45px] bg-gray-700 border-gray-800 text-white'
                   color="default"
-                  variant="faded"
+                  variant="bordered"
+                  type='submit'
                 >
                   Log in
                 </Button>
-              </div>
+              </form>
               <div className="w-full flex gap-2 pt-8 center text-sm">
                 <span>
                   Dont&apos;t have an account?
                 </span>
-                <Link to='/register' className='font-bold'>Click here!</Link>
+                <Link to='/register' className='font-bold'>Sign up!</Link>
               </div>
             </div>
           </div>
