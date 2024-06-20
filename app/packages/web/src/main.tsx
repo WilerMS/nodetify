897b2bb1c094da691f85b7f445e9392740dabddf
WilerMS <wilersoriano@gmail.com>
2024-06-21 00:43:51 +0200

    add providers

--- app/packages/web/src/main.tsx
+++ app/packages/web/src/main.tsx
@@ -1,16 +1,31 @@
+import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { NextUIProvider } from '@nextui-org/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-import App from './App'
+import { store } from '@/redux/store'
 import './index.css'
 
 const root = document.getElementById('root') as HTMLDivElement
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
+
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </QueryClientProvider>
     </NextUIProvider>
   </React.StrictMode>
 )
