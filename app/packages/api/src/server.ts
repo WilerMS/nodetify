c4f4358b919b0a275f8be34d1a6a186d09f70ea1
WilerMS <wilersoriano@gmail.com>
2024-06-25 00:00:00 +0200

    add main function

--- app/packages/api/src/server.ts
+++ app/packages/api/src/server.ts
@@ -8,31 +8,41 @@ import cookieParser from 'cookie-parser'
 import { PORT } from '@/config/env'
 import { errorMiddleware } from '@/middlewares'
 import { apiV1Router } from '@/routes/v1'
+import { initPostgresDbs } from '@/services/databases-service/postgres'
 
-const app = express()
+const main = async () => {
+  const app = express()
 
-// Middlewares
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(cookieParser())
+  // Middlewares
+  app.use(cors())
+  app.use(morgan('dev'))
+  app.use(express.json())
+  app.use(cookieParser())
 
-// static files
-const publicUrl = path.join(__dirname, '../../', 'web/dist')
-app.use(express.static(publicUrl))
+  // static files
+  const publicUrl = path.join(__dirname, '../../', 'web/dist')
+  app.use(express.static(publicUrl))
 
-app.get('/', (_, res) => {
-  res.sendFile(path.join(__dirname, publicUrl, 'index.html'))
-})
+  app.get('/', (_, res) => {
+    res.sendFile(path.join(__dirname, publicUrl, 'index.html'))
+  })
 
-// authentication
+  // authentication
 
-// api Routes
-app.use('/api/v1', apiV1Router)
+  // api Routes
+  app.use('/api/v1', apiV1Router)
 
-// Error handling
-app.use(errorMiddleware)
+  // Error handling
+  app.use(errorMiddleware)
 
-app.listen(PORT, () => {
-  console.log(`Server on port ${PORT}`)
-})
+  // Init services
+  await initPostgresDbs()
+
+  app.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`)
+  })
+
+  return app
+}
+
+main()
