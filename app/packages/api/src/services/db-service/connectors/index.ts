5fa6c8ccd77e78ddd3f1c50e589c1629bad8c958
WilerMS <wilersoriano@gmail.com>
2024-07-05 23:15:30 +0200

    add connectors

---
+++ app/packages/api/src/services/db-service/connectors/index.ts
@@ -0,0 +1,20 @@
+import { type Database } from '@/models'
+import { PgConnector } from './PgConnector'
+
+export const createDatabaseConnection = (database: Database) => {
+  if (database.type === 'PostgreSQL') {
+    return new PgConnector(database.id, database.connection)
+  }
+  /* if (database.type === 'MySQL') {
+    return new DB()
+  } */
+
+  throw new Error(`
+    Error during connection for the next database:
+    ID: ${database.id}
+    Name: ${database.name}
+    Host: ${database.connection.host}
+    Port: ${database.connection.port}
+    User: ${database.connection.user}
+  `)
+}
