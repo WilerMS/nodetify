11d7451d8541eba8f17675d15e0d97d78857af02
WilerMS <wilersoriano@gmail.com>
2024-07-06 02:41:15 +0200

    completing start db service invoquer

--- app/packages/api/src/server.ts
+++ app/packages/api/src/server.ts
@@ -8,7 +8,8 @@ import cookieParser from 'cookie-parser'
 import { PORT } from '@/config/env'
 import { errorMiddleware } from '@/middlewares'
 import { apiV1Router } from '@/routes/v1'
-import { initPostgresDbs } from '@/services/databases-service/postgres'
+import { startDbService } from './services/db-service'
+import { Database } from './models'
 
 const main = async () => {
   const app = express()
@@ -35,8 +36,46 @@ const main = async () => {
   // Error handling
   app.use(errorMiddleware)
 
-  // Init services
-  await initPostgresDbs()
+  // TODO: Refactor this code and move it to separated functions
+  startDbService({
+    events: {
+      onClientConnected: async (conn) => {
+        const schema = await conn.getSchema()
+        const data = {
+          schema: JSON.stringify(schema),
+          status: 'active' as const,
+          last_checked_at: new Date().toISOString()
+        }
+        await Database.query().findById(conn.id).patch(data)
+      },
+      onClientNotification: (notification) => {
+
+      },
+      onCheckConnectionSuccess: async (conn) => {
+        const schema = await conn.getSchema()
+        const data = {
+          schema: JSON.stringify(schema),
+          status: 'active' as const,
+          last_checked_at: new Date().toISOString()
+        }
+        await Database.query().findById(conn.id).patch(data)
+      },
+      onClientError: async (conn) => {
+        const data = {
+          status: 'error' as const,
+          last_checked_at: new Date().toISOString()
+        }
+        await Database.query().findById(conn.id).patch(data)
+      },
+      onCheckConnectionError: async (conn) => {
+        const data = {
+          status: 'error' as const,
+          last_checked_at: new Date().toISOString()
+        }
+        await Database.query().findById(conn.id).patch(data)
+      }
+    }
+  })
 
   app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`)
