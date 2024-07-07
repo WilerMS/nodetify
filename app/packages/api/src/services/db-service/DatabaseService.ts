06c1a46c39519fc71f05c1fb6a3f7f6d2fa3cb50
WilerMS <wilersoriano@gmail.com>
2024-07-07 15:02:10 +0200

    implement a service for Databases connections

---
+++ app/packages/api/src/services/db-service/DatabaseService.ts
@@ -0,0 +1,69 @@
+import { type ConnectionHandler } from './handlers'
+import { createDatabaseConnection } from './connectors'
+import { type IDatabase } from './interfaces/IDatabase'
+import { type Database } from '@/models'
+import { logger } from '@/utils'
+import { type IEventHandlers } from './interfaces/IEventHandlers'
+
+interface Options {
+  events?: IEventHandlers
+}
+
+export class DatabaseService {
+  connectionHandler!: ConnectionHandler
+  databaseModel!: IDatabase
+  options!: Options
+
+  constructor (
+    connectionHandler: ConnectionHandler,
+    databaseModel: IDatabase,
+    options: Options
+  ) {
+    this.connectionHandler = connectionHandler
+    this.databaseModel = databaseModel
+    this.options = options
+  }
+
+  async start () {
+    const databases = await this.databaseModel.query()
+    for (const database of databases) {
+      this.addDatabase(database)
+    }
+  }
+
+  async addDatabase (database: Database) {
+    const connection = createDatabaseConnection(database)
+    const { events } = this.options
+
+    connection.on('logger.info', info => logger.info(info))
+    connection.on('logger.error', info => logger.error(info))
+    connection.on('logger.warn', info => logger.warn(info))
+
+    connection.on('client.connected', connection => {
+      events?.onClientConnected?.(connection)
+    })
+
+    connection.on('client.connecting', connection => {
+      events?.onClientConnecting?.(connection)
+    })
+
+    connection.on('client.error', data => {
+      events?.onClientError?.(data)
+    })
+
+    connection.on('client.notification', data => {
+      events?.onClientNotification?.(data)
+    })
+
+    connection.on('client.checkConnection.success', data => {
+      events?.onCheckConnectionSuccess?.(data)
+    })
+
+    connection.on('client.checkConnection.error', data => {
+      events?.onCheckConnectionError?.(data)
+    })
+
+    // Add connection to the storage
+    this.connectionHandler.addConnection(database.id, connection)
+  }
+}
