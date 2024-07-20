199259ca1d5f01bf14a9c2ad6d1a409ea2a0562c
WilerMS <wilersoriano@gmail.com>
2024-07-21 00:18:36 +0200

    refactor alarm service

--- app/packages/api/src/services/alarms-service/AlarmService.ts
+++ app/packages/api/src/services/alarms-service/AlarmService.ts
@@ -1,84 +1,67 @@
-import { type DatabaseService } from '../db-service/DatabaseService'
-import { type IAlarm } from './interfaces/IAlarm'
-
-type AlarmsMap = Map<number, IAlarm>
-type Cache = Map<number, Map<string, AlarmsMap>>
+import { type DbClientNotification } from '../db-service/connectors'
+import { type DatabaseId, type TableName, type IAlarm, type IAlarmServiceCache } from './interfaces'
 
 export class AlarmService {
-  cache!: Cache
-  dbService!: DatabaseService
+  private readonly cache: IAlarmServiceCache = new Map()
 
-  constructor(dbService: DatabaseService) {
-    this.dbService = dbService
-    this.cache = new Map()
-  }
-
-  start(alarms: IAlarm[]) {
+  constructor(alarms: IAlarm[]) {
     for (const alarm of alarms) {
-      this.addAlarm(alarm)
+      this.addOrReplaceAlarm(alarm)
     }
   }
 
-  getTableAlarms(databaseId: number, tablename: string) {
-    const database = this.cache.get(databaseId)
-    if (!database) throw new Error('Database connection not found')
-
-    const tableAlarms = database?.get(tablename)
-    if (!tableAlarms) throw new Error('Tablename not found in cache storage')
-
-    return Array.from(tableAlarms?.values())
+  getTableAlarms(dbId: DatabaseId, tablename: TableName) {
+    const tableAlarms = this.getTableCache(dbId, tablename)
+    return Array.from(tableAlarms.values())
   }
 
-  addAlarm(alarm: IAlarm) {
+  addOrReplaceAlarm(alarm: IAlarm) {
     const { database_id: dbId, table_name: tablename, id } = alarm
-
-    if (!this.cache.has(dbId)) {
-      this.cache.set(dbId, new Map())
-    }
-
-    if (!this.cache.get(dbId)?.has(tablename)) {
-      this.cache.get(dbId)?.set(tablename, new Map())
-    }
-
-    this.cache.get(dbId)?.get(tablename)?.set(id, alarm)
-
-    // Add the current alarm to notification evennt of its own database connection
-    this.registerAlarmToDatabaseService(alarm)
+    const tablesCache = this.getOrCreateTableCache(dbId, tablename)
+    return tablesCache.set(id, alarm)
   }
 
   removeAlarm(alarm: IAlarm) {
-    this.cache
-      .get(alarm.database_id)
-      ?.get(alarm.table_name)
-      ?.delete(alarm.id)
+    const tablesCache = this.getTableCache(alarm.database_id, alarm.table_name)
+    return tablesCache.delete(alarm.id)
   }
 
-  // Method used to verify alarms when a notification gets triggered
-  /* checkAlarms(notification: DbClientNotification) {
-    const { databaseId, table, type, data } = notification
-
-    const alarms = this.getTableAlarms(databaseId, table)
-
-    console.log('Procesing notification in Alarm service')
-    console.log(alarms)
+  checkAlarm(notification: DbClientNotification) {
+    const alarms = this.getTableAlarms(notification.databaseId, notification.table)
+    console.log(alarms[0].condition)
     console.log(notification)
-  } */
+  }
 
-  /**
-   * Method to register alarms to the notification events on each connection
-   * @param alarm IAlarm
-   */
-  async registerAlarmToDatabaseService(alarm: IAlarm) {
-    try {
-      const connection = this.dbService.getConnection(alarm.database_id)
-
-      await connection.injectTableTrigger(alarm.table_name)
-
-      connection.on('client.notification', (notification) => {
-        console.log('llegó la notificación. Que vaina', notification)
-      })
-    } catch (error) {
-      console.log(error)
+  private getDatabaseCache(databaseId: DatabaseId) {
+    const databaseCache = this.cache.get(databaseId)
+    if (!databaseCache) {
+      throw new Error('Database connection not found.')
     }
+    return databaseCache
+  }
+
+  private getTableCache(databaseId: DatabaseId, tablename: TableName) {
+    const databaseCache = this.getDatabaseCache(databaseId)
+    const tablesCache = databaseCache.get(tablename)
+    if (!tablesCache) {
+      throw new Error('Tablename not found in cache storage')
+    }
+    return tablesCache
+  }
+
+  private getOrCreateTableCache(databaseId: DatabaseId, tablename: TableName) {
+    let databaseCache = this.cache.get(databaseId)
+    if (!databaseCache) {
+      databaseCache = new Map()
+      this.cache.set(databaseId, databaseCache)
+    }
+
+    let tablesCache = databaseCache.get(tablename)
+    if (!tablesCache) {
+      tablesCache = new Map()
+      databaseCache.set(tablename, tablesCache)
+    }
+
+    return tablesCache
   }
 }
