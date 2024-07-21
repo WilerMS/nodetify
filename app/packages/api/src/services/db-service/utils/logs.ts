936ceab78bd7dddb3e04de522f3068fc6589330b
WilerMS <wilersoriano@gmail.com>
2024-07-21 02:52:23 +0200

    add a new log messages for triggers

--- app/packages/api/src/services/db-service/utils/logs.ts
+++ app/packages/api/src/services/db-service/utils/logs.ts
@@ -11,5 +11,6 @@ export const DB_LOG_MESSAGES = {
   CLIENT_CHECK_CONNECTION_ATTEMPT: (conn: DBConnector) => `[${conn.getClassName()}] - Checking connection for database ${conn.config.database} with ID ${conn.id} at ${conn.config.host}:${conn.config.port}.`,
   CLIENT_CHECK_CONNECTION_SUCCESS: (conn: DBConnector) => `[${conn.getClassName()}] - Connection for database ${conn.config.database} at ${conn.config.host}:${conn.config.port} with ID ${conn.id} remains active.`,
   CLIENT_CHECK_CONNECTION_ERROR: (conn: DBConnector) => `[${conn.getClassName()}] - Connection lost for database ${conn.config.database} at ${conn.config.host}:${conn.config.port} with ID ${conn.id}, reconnecting...`,
-  CLIENT_TRIGGER_INJECTION_ERROR: (conn: DBConnector) => `[${conn.getClassName()}] - Failed to inject nodetify trigger in database ${conn.config.database} at ${conn.config.host}:${conn.config.port} with ID ${conn.id}, reconnecting...`
+  CLIENT_TRIGGER_INJECTION_ERROR: (conn: DBConnector) => `[${conn.getClassName()}] - Failed to inject nodetify trigger in database ${conn.config.database} at ${conn.config.host}:${conn.config.port} with ID ${conn.id}.`,
+  CLIENT_TRIGGER_INJECTION_SUCCESS: (conn: DBConnector) => `[${conn.getClassName()}] - Trigger successfully injected to database ${conn.config.database} at ${conn.config.host}:${conn.config.port} with ID ${conn.id}.`
 }
