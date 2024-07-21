c837a442e813da1c2e6d5d80c9abd929196bd48e
WilerMS <wilersoriano@gmail.com>
2024-07-21 02:53:44 +0200

    add alarm service veryfiers

---
+++ app/packages/api/src/services/alarms-service/utils/index.ts
@@ -0,0 +1,16 @@
+import { type Condition } from '@/models'
+import { type Column } from '@/services/db-service/utils'
+
+export const validateAlarmConditionOperator = (condition: Condition, column: Column) => {
+  if (column.type === 'boolean' && ['=', '!='].includes(condition.operator)) return true
+  if (column.type === 'number' && ['=', '!=', '>', '<', '>=', '<='].includes(condition.operator)) return true
+  if (column.type === 'string' && ['=', '!='].includes(condition.operator)) return true
+  return false
+}
+
+export const validateAlarmConditionValue = (condition: Condition, column: Column) => {
+  if (column.type === 'boolean' && typeof condition.value === 'boolean') return true
+  if (column.type === 'number' && typeof condition.value === 'number') return true
+  if (column.type === 'string' && typeof condition.value === 'string') return true
+  return false
+}
