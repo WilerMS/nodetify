550294768933046bda7ecc752dcddea22226c2bf
WilerMS <wilersoriano@gmail.com>
2024-07-24 01:57:49 +0200

    centralize types in index

--- app/packages/api/src/services/alarms-service/interfaces/IAlarm.ts
+++
@@ -1,4 +0,0 @@
-import { type Alarm } from '@/models'
-
-export type IAlarmModel = typeof Alarm
-export interface IAlarm extends Alarm {}
--- app/packages/api/src/services/alarms-service/interfaces/index.ts
+++ app/packages/api/src/services/alarms-service/interfaces/index.ts
@@ -1,2 +1,12 @@
-export * from './IAlarm'
-export * from './IAlarmServiceCache'
+import { type Alarm } from '@/models'
+
+export type IAlarmModel = typeof Alarm
+export interface IAlarm extends Alarm {}
+
+export type DatabaseId = number
+export type TableName = string
+export type AlarmId = number
+
+export type AlarmsMap = Map<AlarmId, IAlarm>
+export type TablesMap = Map<TableName, AlarmsMap>
+export type IAlarmServiceCache = Map<DatabaseId, TablesMap>
