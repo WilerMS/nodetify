23bee711f289f4df486ffc44fc1f25824fbd3747
WilerMS <wilersoriano@gmail.com>
2024-07-24 02:09:19 +0200

    refactor crypto utility

--- app/packages/api/src/utils/crypto.ts
+++ app/packages/api/src/utils/crypto.ts
@@ -1,12 +1,11 @@
 import crypto from 'crypto'
 
-const algorithm = 'aes-256-ctr'
-
+const ALGORITHM = 'aes-256-ctr'
 export type EncryptedText = `${string}:${string}`
 
 export const encrypt = (secretKey: string, text: string): EncryptedText => {
   const iv = crypto.randomBytes(16)
-  const cipher = crypto.createCipheriv(algorithm, Buffer.from(secretKey, 'hex'), iv)
+  const cipher = crypto.createCipheriv(ALGORITHM, Buffer.from(secretKey, 'hex'), iv)
   const encrypted = Buffer.concat([cipher.update(text), cipher.final()])
 
   return `${iv.toString('hex')}:${encrypted.toString('hex')}`
@@ -14,7 +13,7 @@ export const encrypt = (secretKey: string, text: string): EncryptedText => {
 
 export const decrypt = (secretKey: string, text: EncryptedText) => {
   const [iv, content] = text.split(':')
-  const decipher = crypto.createDecipheriv(algorithm, Buffer.from(secretKey, 'hex'), Buffer.from(iv, 'hex'))
+  const decipher = crypto.createDecipheriv(ALGORITHM, Buffer.from(secretKey, 'hex'), Buffer.from(iv, 'hex'))
   const decrypted = Buffer.concat([decipher.update(Buffer.from(content, 'hex')), decipher.final()])
 
   return decrypted.toString()
